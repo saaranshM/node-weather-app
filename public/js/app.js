ebca277d4147f13a68018f64bedcd49af2729743
saaranshM <saaransh.menon2000@gmail.com>
2021-05-23 23:58:54 +0530

    added more data for weather

--- public/js/app.js
+++ public/js/app.js
@@ -2,6 +2,7 @@ const weatherForm = document.querySelector("form");
 const search = document.querySelector("input");
 const messageOne = document.getElementById("message-1");
 const messageTwo = document.getElementById("message-2");
+const messageThree = document.getElementById("message-3");
 
 weatherForm.addEventListener("submit", (e) => {
   e.preventDefault();
@@ -10,6 +11,7 @@ weatherForm.addEventListener("submit", (e) => {
 
   messageOne.textContent = "Loading....";
   messageTwo.textContent = "";
+  messageThree.textContent = "";
 
   fetch(`/weather?address=${location}`).then((response) => {
     messageOne.textContent = "";
@@ -18,7 +20,8 @@ weatherForm.addEventListener("submit", (e) => {
         return (messageOne.textContent = data.error);
       }
       messageOne.textContent = data.location;
-      messageTwo.textContent = data.forecast;
+      messageTwo.textContent = data.forecast.lineOne;
+      messageThree.textContent = data.forecast.lineTwo;
     });
   });
 });
--- src/utils/forecast.js
+++ src/utils/forecast.js
@@ -1,26 +1,30 @@
-const axios = require('axios')
+const axios = require("axios");
 
+const access_key = "0c307cc09c0d0361ea3c5960689c3481";
 
-const access_key = '0c307cc09c0d0361ea3c5960689c3481'
+const forecast = (lat, long, callback) => {
+  const weather_url = "http://api.weatherstack.com/current";
 
-const forecast = (lat,long,callback) => {
-    
-    const weather_url = 'http://api.weatherstack.com/current'
+  axios
+    .get(weather_url, {
+      params: {
+        access_key,
+        query: lat + "," + long,
+      },
+    })
+    .then(({ status, data }) => {
+      if (status == 200) {
+        callback(undefined, {
+          lineOne: `${data.current.weather_descriptions[0]}. It is currently ${data.current.temperature} degrees out and it feels like ${data.current.feelslike} degrees out.`,
+          lineTwo: `With humdity of ${data.current.humidity}.`,
+        });
+      } else {
+        callback("API Error!");
+      }
+    })
+    .catch((e) => {
+      callback("User Error!");
+    });
+};
 
-    axios.get(weather_url, {
-        params: {
-            access_key,
-            query: lat + "," + long,
-        }
-    }).then(({status, data}) => {
-        if (status == 200){
-            callback(undefined,`${data.current.weather_descriptions[0]}. It is currently ${data.current.temperature} degrees out. It feels like ${data.current.feelslike} degrees out.`)
-        } else {
-            callback("API Error!")
-        }
-    }).catch((e) => {
-        callback("User Error!")
-    })    
-}
-
-module.exports = forecast
+module.exports = forecast;
